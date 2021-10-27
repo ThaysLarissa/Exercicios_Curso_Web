97bde5d49c57a22af30252a754b7255a27ee1d9b
ThaysLarissa <thayslarissa96@gmail.com>
2021-10-26 21:50:19 -0300

    Atividades realizadas nas aulas EAD e Presencial

---
+++ Aula_26102021/comentario-js_aula_26102021.js
@@ -0,0 +1,5 @@
+//Para comentar no JS, e preciso colocar duas barras para inicia o comentário e duas barras invertidas após o texto do comentario\\
+
+
+
+/*Outro modo de comentar utilizando barra >> asterisco >> asterisco >> barra e o texto do comentário entre os asteriscos*/
